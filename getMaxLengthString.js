be71017d92b5e10e2de13c0b342bbdff5e567cb7
sealbb <chonticha.it@mail.kmutt.ac.th>
2022-09-07 15:54:25 +0700

    help

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -3,5 +3,6 @@ const { template } = require('@babel/core')
 function getMaxLengthString(arrayOfString) {
   //code here
   console.log("Help")
+  console.log("Help")
 }
 module.exports = getMaxLengthString
