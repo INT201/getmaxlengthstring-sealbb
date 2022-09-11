f32771667e80d83edbfdea84187c84a8446592ff
Chonticha <Chonticha.it@mail.kmutt.ac.th>
2022-09-11 10:15:20 +0700

    update

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -2,13 +2,6 @@ const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
   //code here
-  // if(arrayOfString===(null || undefined)){
-  //   return undefined
-  // }else if(arrayOfString!==null||undefined){
-  //   let count = arrayOfString.map(word => word.length)
-  //   let max = Math.max(...count)
-  //   return arrayOfString.filter(word => word.length===max)
-  // }
   if(!arrayOfString){
      return undefined
   }
